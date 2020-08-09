cea343270174bea7dd757f839cd734c96d52d637
Aloy23 <aloysiustws@gmail.com>
2020-08-09 21:26:50 +0800

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -43,7 +43,7 @@ export class LoginPage implements OnInit {
 
   login() {
     // tslint:disable-next-line: quotemark
-    if (this.email === "" || this.password === "" ) {
+    if (this.email == "" || this.password == "") {
       this.presentToast('Please enter your email and password!', 'middle', 2000);
     }
     else {
