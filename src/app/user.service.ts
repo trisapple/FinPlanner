22184c242ec053b7420564a49217893ff0a3cb1d
Aloy23 <aloysiustws@gmail.com>
2020-08-13 22:04:14 +0800

    Update user.service.ts

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -43,15 +43,4 @@ export class UserService {
     this.firestore.collection<any>('users').doc(email).delete();
     this.loggedin = false;
   }
-
-
-  // loginWithFacebook(email: string) {
-  // tslint:disable-next-line: max-line-length
-  //   return this.firestore.collection<any>('users').doc(email).valueChanges(); // Get the email (document) of the user, so we can get the name, email, password
-  // }
-
-  // loginWithGoogle(email: string) {
-  // tslint:disable-next-line: max-line-length
-  //   return this.firestore.collection<any>('users').doc(email).valueChanges(); // Get the email (document) of the user, so we can get the name, email, password
-  // }
 }
