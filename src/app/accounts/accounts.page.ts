7675c321528ea563771afc7d63803d8bc83f2983
Aloy23 <aloysiustws@gmail.com>
2020-08-13 19:52:35 +0800

    Change API Provider

--- src/app/accounts/accounts.page.ts
+++ src/app/accounts/accounts.page.ts
@@ -150,8 +150,8 @@ export class AccountsPage implements OnInit {
     window.open("https://sandbox.apihub.citi.com/gcb/api/authCode/oauth2/authorize?response_type=code&client_id=05451865-7d39-4704-b495-803f11d2dd09&scope=accounts_details_transactions&countryCode=SG&businessCode=GCB&locale=en_SG&state=12093&redirect_uri=http://ionicfirebase-a8213.web.app", "_blank");
   }
 
-  dbsconnect() {
-    window.open("https://www.dbs.com/sandbox/api/sg/v1/oauth/authorize?client_id=75fd953a-e032-4525-8deb-ca0800a2c08c&scope=Read&response_type=code&redirect_uri=https://ionicfirebase-a8213.web.app", "_blank");
+  ocbcconnect() {
+    window.open("https://api.ocbc.com/ocbcauthentication/api/oauth2/authorize?client_id=Bdf48cJM_OdAilo6j_kBn_PhQLwa&redirect_uri=https://ionicfirebase-a8213.web.app/&scope=transactional", "_blank");
   }
 
   transactionhistory(account) {
