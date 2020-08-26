5ee6003c66cd7c5f745f8547e05bc4e11ce91a63
Tristan Chng <tristan.chng@gmail.com>
2020-08-26 10:54:14 +0800

    Add to-do to firebase

--- src/app/todolist/addtodo/addtodo.page.ts
+++ src/app/todolist/addtodo/addtodo.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { TodoListService } from 'src/app/todo-list.service';
 
 @Component({
   selector: 'app-addtodo',
@@ -8,12 +10,20 @@ import { NavController } from '@ionic/angular';
 })
 export class AddtodoPage implements OnInit {
 
-  constructor(public navCtrl: NavController) { }
+  name: String
+
+  constructor(public navCtrl: NavController, public firestore: AngularFirestore, public todolistService: TodoListService) { }
 
   ngOnInit() {
   }
 
   addTodo() {
+    var reminder = {}
+    console.log(this.todolistService.todoList)
+    reminder["name"] = this.name
+    this.todolistService.todoList.push(reminder)
+    console.log(this.name)
+    this.firestore.collection<any>('users').doc("1802328C@student.tp.edu.sg").update({todolist: this.todolistService.todoList})
     this.navCtrl.pop()
   }
 
