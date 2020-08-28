e7ec5be7b85f8e21ea994895eefe79f28a6b8bd2
Tristan Chng <tristan.chng@gmail.com>
2020-08-28 10:48:40 +0800

    Merged date and time picker and check if reminder name and due date is filled in

--- src/app/todolist/addtodo/addtodo.page.ts
+++ src/app/todolist/addtodo/addtodo.page.ts
@@ -13,9 +13,9 @@ export class AddtodoPage implements OnInit {
 
   name: String
   date: Date
-  time: Time
+  // time: Time
 
-  constructor(public navCtrl: NavController, public firestore: AngularFirestore, public todolistService: TodoListService) { 
+  constructor(public navCtrl: NavController, public firestore: AngularFirestore, public todolistService: TodoListService) {
 
   }
 
@@ -23,32 +23,37 @@ export class AddtodoPage implements OnInit {
   }
 
   addTodo() {
-    var todo = {} // Temporary New todo Object
-    console.log(this.todolistService.todoList)
-    todo["name"] = this.name // Set the name of the New todo Object
-    todo["dueDate"] = new Date() // Set the date of the New todo Object to today's date for now
-    this.todolistService.todoList.push(todo) // Add the newly added todo to the todoList array
-    
-    // For logging
-    console.log(todo["dueDate"])
-    console.log(todo)
-    console.log(this.name)
-    console.log(this.date)
-    console.log(this.time)
+    if (this.name == undefined || this.name == "" || this.date == undefined) {
+      alert("Please enter a reminder name and date to be reminded")
+    } else {
+      var todo = {} // Temporary New todo Object
+      console.log(this.todolistService.todoList)
+      todo["name"] = this.name // Set the name of the New todo Object
+      todo["dueDate"] = new Date(this.date) // Set the date of the New todo Object.
+      this.todolistService.todoList.push(todo) // Add the newly added todo to the todoList array
 
-    // Update the user's todoList with the newly added todo added to the todoList array
-    this.firestore.collection<any>('users').doc("1802328C@student.tp.edu.sg").update({
-      todolist: this.todolistService.todoList
-    })
-    // After it is updated, refresh the todolist and go back.
-    .then(value => {
-      this.todolistService.getTodo()
-      this.navCtrl.pop()
-    })
-    // Log and catch the error
-    .catch(value => {
-      console.log(value)
-    })
+      // For logging
+      console.log(todo["dueDate"])
+      console.log(todo)
+      console.log(this.name)
+      console.log(this.date)
+      // console.log(this.time)
+
+      // Update the user's todoList with the newly added todo added to the todoList array
+      this.firestore.collection<any>('users').doc("1802328C@student.tp.edu.sg").update({
+        todolist: this.todolistService.todoList
+      })
+        // After it is updated, refresh the todolist and go back.
+        .then(value => {
+          this.todolistService.getTodo()
+          this.navCtrl.pop()
+        })
+        // Log and catch the error
+        .catch(value => {
+          console.log(value)
+        })
+    }
+
   }
 
 }
