cbd2527328861368c95102f03d4b3f72fb2c7e7a
Aloy23 <aloysiustws@gmail.com>
2020-09-01 22:51:36 +0800

    Shifted "Savings Suggestions" into Accounts and added onlick

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,8 @@ const routes: Routes = [
   { path: 'faq/askquestions', loadChildren: () => import('./faq/askquestions/askquestions.module').then(m => m.AskquestionsPageModule) },
   { path: 'todolist/add', loadChildren: () => import('./todolist/addtodo/addtodo.module').then(m => m.AddtodoPageModule) },
   { path: 'todolist/update', loadChildren: () => import('./todolist/updatetodo/updatetodo.module').then(m => m.UpdatetodoPageModule) },
-  { path: 'savingssuggestion', loadChildren: () => import('./savingssuggestion/savingssuggestion.module').then( m => m.SavingssuggestionPageModule) }
+  { path: 'accounts/savingssuggestion', loadChildren: () => import('./accounts/savingssuggestion/savingssuggestion.module').then( m => m.SavingssuggestionPageModule) },
+  
 
 ];
 
