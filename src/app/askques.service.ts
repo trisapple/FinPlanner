f83486190924adb7c93b8a591934840e28c86cf8
fishsoupp <50898157+fishsoupp@users.noreply.github.com>
2020-09-06 23:20:43 +0800

    get the askques to work but trying to work on the uid

--- src/app/askques.service.ts
+++ src/app/askques.service.ts
@@ -1,57 +1,32 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Subscription } from 'rxjs';
-import { UserService } from './user.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AskQuesService {
 
-  todoList = []
+  uid: string;
+  username: string;
+  email: string;
+  ques: string;
+  // usersquesCollectionRef: AngularFirestoreCollection<any>;
 
-  // Update todo
-  name: String
-  date: string
-  index: any
-  checked: Boolean
-
-  constructor(public firestore: AngularFirestore, public userService: UserService) {
-    // this.usersCollectionRef = this.firestore.collection<any>('users'); // Get the 'users' collection in Firebase Cloud Firestore
+  constructor(public firestore: AngularFirestore) {
+    // this.usersquesCollectionRef = this.firestore.collection<any>('usersques');
   }
 
-  getTodo() {
-    let sub: Subscription = this.firestore.collection<any>('users').doc(this.userService.uid).valueChanges().subscribe((data) => {
-      console.log(data)
-      console.log(this.userService.uid)
-      console.log(data["todolist"])
+  addUpdateContact(username: string, email: string, ques: string, uid: string){
+    this.firestore.collection<any>('users').doc(uid).set({username: username, email: email, ques: ques});
+    }
 
-      // If the user has no todolist variable, create it for them
-      if (data["todolist"] == undefined) {
-        console.log("Data undefined run")
-        this.firestore.collection<any>('users').doc(this.userService.uid).update({
-          todolist: this.todoList
-        })
-      }
+  deleteContact(name: string){
+    this.firestore.collection<any>('users').doc(name).delete();
+    }
 
-      // If the user has an existing todolist variable, put it into the todoList array to display it in html.
-      else {
-        this.todoList = data["todolist"]
-      }
+  getContacts() {
+    return this.firestore.collection<any>('users').doc().valueChanges();
+    }
 
-      // For logging
-      console.log(this.todoList)
-      for (let each of this.todoList) {
-        console.log(each)
-        console.log(each.name)
-        console.log(each.dueDate)
-      }
-
-      sub.unsubscribe();
-    });
-  }
-
-  // gettodoList() {
-  //   return this.firestore.collection<any>('users').doc("1802328C@student.tp.edu.sg").valueChanges();
-  // }
 }
--- src/app/faq/askquestions/askquestions.page.ts
+++ src/app/faq/askquestions/askquestions.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController, ToastController } from '@ionic/angular';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AskQuesService } from 'src/app/askques.service';
+import { UserService } from 'src/app/user.service';
+import { FormBuilder, FormGroup} from '@angular/forms';
+
 
 @Component({
   selector: 'app-askquestions',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AskquestionsPage implements OnInit {
 
-  constructor() { }
+  myForm: FormGroup;
+  msg: string;
+
+  constructor(public navCtrl: NavController,public builder: FormBuilder, public firestore: AngularFirestore, public askquesService: AskQuesService, public toastCtrl: ToastController, public userService: UserService) {
+    this.myForm = builder.group({
+      username: '',
+      email: '',
+      ques : '',
+      });
+  }
 
   ngOnInit() {
   }
 
+  clickInsert(res) {
+    if (this.myForm.value.username != "" && this.myForm.value.email !="" && this.myForm.value.ques !="") {
+      this.askquesService.addUpdateContact(this.askquesService.username, this.askquesService.email, this.askquesService.ques, res.user.uid);
+      this.presentToast('Question sent successfully');
+      }
+      else {
+      this.presentToast('Please fill in all text fields');
+      }
+  }
+
+ clickView () {
+  this.navCtrl.push (QueslistPage);
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+
+  }
 }
