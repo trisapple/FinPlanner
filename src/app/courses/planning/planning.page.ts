04468df45f118b3ca120761565fb0c159e0b2d25
fishsoupp <50898157+fishsoupp@users.noreply.github.com>
2020-09-11 17:01:02 +0800

    Added Pictures and page

---
+++ src/app/courses/planning/planning.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planning',
+  templateUrl: './planning.page.html',
+  styleUrls: ['./planning.page.scss'],
+})
+export class PlanningPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
