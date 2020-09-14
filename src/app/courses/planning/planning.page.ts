6bab4918cc0dc488367aad7b40e89307c2ae54c7
Tristan Chng <tristan.chng@gmail.com>
2020-09-14 10:11:12 +0800

    Update planning.page.ts
    Added vid:<any> to resolve Type 'SafeResourceUrl' is not assignable to type 'string'.

--- src/app/courses/planning/planning.page.ts
+++ src/app/courses/planning/planning.page.ts
@@ -10,28 +10,28 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class PlanningPage implements OnInit {
   public items = [
     {
-      vid: 'https://www.youtube.com/embed/ggv21pNgbtM',
+      vid:<any> 'https://www.youtube.com/embed/ggv21pNgbtM',
       sub: 'LESSON 1',
       head: 'What is Financial Planning?',
       des: 'In this video, you will be learning what is financial planning.',
       time: '3 mins'
     },
     {
-      vid: 'https://www.youtube.com/embed/aXDuLxEJqBo',
+      vid:<any> 'https://www.youtube.com/embed/aXDuLxEJqBo',
       sub: 'LESSON 2',
       head: 'How to set and prioritise your financial goals',
       des: 'In this video, you will be learning how to set achievable goals and prioritise them.',
       time: '5 mins'
     },
     {
-      vid: 'https://www.youtube.com/embed/SR8-qWu549c',
+      vid:<any> 'https://www.youtube.com/embed/SR8-qWu549c',
       sub: 'LESSON 3',
       head: 'How to put your financial plan into action',
       des: 'In this video, you will be learning how to put your plan into action and achieving them.',
       time: '6 mins'
     },
     {
-      vid: 'https://www.youtube.com/embed/CU4l_rs50Kk',
+      vid:<any> 'https://www.youtube.com/embed/CU4l_rs50Kk',
       sub: 'LESSON 4',
       head: 'Financial Planning Basics: Key Summary',
       des: 'In this video, we will summarise what we have gone through in the previous 3 videos.',
