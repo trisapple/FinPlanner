85f1e8ebb58d24d4a4c67702a419e9d673c00350
fishsoupp <50898157+fishsoupp@users.noreply.github.com>
2020-09-19 17:32:39 +0800

    Put info into other courses page

--- src/app/courses/invest/invest.page.ts
+++ src/app/courses/invest/invest.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-invest',
@@ -6,8 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./invest.page.scss'],
 })
 export class InvestPage implements OnInit {
+  public items = [
+    {
+      vid:<any> 'https://www.youtube.com/embed/ggv21pNgbtM',
+      sub: 'LESSON 1',
+      head: 'What is an asset class',
+      des: 'In this video, you will be learning what asset classes are.',
+      time: '3 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/aXDuLxEJqBo',
+      sub: 'LESSON 2',
+      head: 'Diversifying your portfolio & Understanding risk',
+      des: 'In this video, you will be learning how much risk you should take depends on factors specific to your own situation. Being honest with yourself about these factors is a key to successful investing.',
+      time: '5 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/SR8-qWu549c',
+      sub: 'LESSON 3',
+      head: 'How do fees impact your returns',
+      des: 'In this video, you will be learning why fees have a bigger impact on your returns than you might think. Here are all the potential fees that you should be aware of.',
+      time: '6 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/CU4l_rs50Kk',
+      sub: 'LESSON 4',
+      head: 'On trying to time the market',
+      des: 'In this video, you will be learning why trying to time the market is one of the most ineffective strategies, and which strategy is better than that.',
+      time: '3 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/CU4l_rs50Kk',
+      sub: 'LESSON 5',
+      head: 'Investing Basics: Key Summary',
+      des: 'In this video, we will summarise what we have gone through in the previous 4 videos.',
+      time: '3 mins'
+    }
+  ];
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {
+
+    this.items.map(el => el.vid = this.sanitizer.bypassSecurityTrustResourceUrl(el.vid));
+    console.log(this.items);
+  }
 
   ngOnInit() {
   }
--- src/app/courses/retire/retire.page.ts
+++ src/app/courses/retire/retire.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-retire',
@@ -6,8 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./retire.page.scss'],
 })
 export class RetirePage implements OnInit {
+  public items = [
+    {
+      vid:<any> 'https://www.youtube.com/embed/ggv21pNgbtM',
+      sub: 'LESSON 1',
+      head: 'What is retirement?',
+      des: 'In this video, you will be learning how to think about planning for your retirement.',
+      time: '3 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/aXDuLxEJqBo',
+      sub: 'LESSON 2',
+      head: 'How much money do you need?',
+      des: 'In this video, you will be learning the various way you can calculate how much you will need for retirement and some of the areas where you might be spending more during retirement.',
+      time: '5 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/SR8-qWu549c',
+      sub: 'LESSON 3',
+      head: 'IS CPF enough?',
+      des: 'In this video, you will be learning how CPF works, how you can use it for your retirement planning and other financial goals.',
+      time: '6 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/CU4l_rs50Kk',
+      sub: 'LESSON 4',
+      head: 'If your CPF is insufficent',
+      des: 'In this video, you will be learning why your CPF may not be sufficient for retirement.',
+      time: '3 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/CU4l_rs50Kk',
+      sub: 'LESSON 5',
+      head: 'What is the Supplementary Retirement Scheme (SRS)?',
+      des: 'In this video, you will be learning how SRS can be a great tool to grow your money. Here\' what you need to know before contributing towards it.',
+      time: '3 mins'
+    },
+    {
+      vid:<any> 'https://www.youtube.com/embed/CU4l_rs50Kk',
+      sub: 'LESSON 6',
+      head: 'How to Plan for Your Retirement: Key Summary',
+      des: 'In this video, we will summarise what we have gone through in the previous 5 videos.',
+      time: '3 mins'
+    }
+  ];
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {
+
+    this.items.map(el => el.vid = this.sanitizer.bypassSecurityTrustResourceUrl(el.vid));
+    console.log(this.items);
+  }
 
   ngOnInit() {
   }
