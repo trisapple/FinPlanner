e051488c064662caffba9cba978dd500568bc624
Aloy23 <aloysiustws@gmail.com>
2020-09-26 17:17:18 +0800

    Comment out "BTC to SGD" coz only can have 5 APIs calls

--- src/app/exchangerates/crypto/crypto.page.ts
+++ src/app/exchangerates/crypto/crypto.page.ts
@@ -43,38 +43,38 @@ export class CryptoPage implements OnInit {
 
       req.end();
 
-      var https = require('follow-redirects').https;
+      // var https = require('follow-redirects').https;
 
-      var options = {
-        'method': 'GET',
-        'hostname': 'www.alphavantage.co',
-        'path': '/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=SGD&apikey=JDK7QWBWHQDIT41Y',
-        'headers': {
-        },
-        'maxRedirects': 20
-      };
+      // var options = {
+      //   'method': 'GET',
+      //   'hostname': 'www.alphavantage.co',
+      //   'path': '/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=SGD&apikey=JDK7QWBWHQDIT41Y',
+      //   'headers': {
+      //   },
+      //   'maxRedirects': 20
+      // };
 
-      var req = https.request(options, function (res) {
-        var chunks = [];
+      // var req = https.request(options, function (res) {
+      //   var chunks = [];
 
-        res.on("data", function (chunk) {
-          chunks.push(chunk);
-        });
+      //   res.on("data", function (chunk) {
+      //     chunks.push(chunk);
+      //   });
 
-        res.on("end", function (chunk) {
-          var body = Buffer.concat(chunks);
-          // console.log(body.toString());
-          console.log (JSON.parse(body.toString())["Realtime Currency Exchange Rate"])
+      //   res.on("end", function (chunk) {
+      //     var body = Buffer.concat(chunks);
+      //     // console.log(body.toString());
+      //     console.log (JSON.parse(body.toString())["Realtime Currency Exchange Rate"])
 
-          expensesService.cryptoSG = JSON.parse(body.toString())["Realtime Currency Exchange Rate"]
-        });
+      //     expensesService.cryptoSG = JSON.parse(body.toString())["Realtime Currency Exchange Rate"]
+      //   });
 
-        res.on("error", function (error) {
-          console.error(error);
-        });
-      });
+      //   res.on("error", function (error) {
+      //     console.error(error);
+      //   });
+      // });
 
-      req.end();
+      // req.end();
 
   }
 
