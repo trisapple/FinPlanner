842c6fda9f260b9b370a2fd0065b818fd9b0d35a
Aloy23 <aloysiustws@gmail.com>
2020-09-27 14:50:37 +0800

    Jing Yu's part: Somewhat got the news API to work. It won't show/load immediately when you go to the news page. You must click on the top headlines, bitcoin segment for it to show up.

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -14,9 +14,11 @@ export class NewsPage implements OnInit {
   page = 1;
   articles: any;
 
-  private selectedSegment: string = 'topheadlines';
+  private selectedSegment: string;
+  // private bitcoinSegment: string = 'bitcoin'
   constructor(private newsService: NewsService, private router: Router, public navCtrl: NavController) { 
     this.loadBBCnews();
+    this.loadbitcoin();
   }
 
   ngOnInit() {
@@ -28,25 +30,26 @@ export class NewsPage implements OnInit {
   }
 
   loadBBCnews() {
-    this.newsService
-    .getData("top-headlines?sources=bbc-news")
-    .subscribe(news => {
-      this.articles = news['articles'];
-      console.log(this.articles);
-    });
+      this.newsService
+      .getData("top-headlines?sources=bbc-news")
+      .subscribe(news => {
+        this.articles = news['articles'];
+        console.log(this.articles);
+      });
   }
 
-  bitcoin() {
-    this.newsService
-      .getData(
-        `everything?q=bitcoin&sortBy=publishedAt${
-          this.page
-        }`
-      )
-      .subscribe(data => {
-        console.log(data);
-        this.data = data;
-      });
+  loadbitcoin() {
+      this.newsService
+        .getData(
+          `everything?q=bitcoin&sortBy=publishedAt${
+            this.page
+          }`
+        )
+        .subscribe(data => {
+          this.articles = data['articles'];
+          console.log(this.articles);
+          this.data = data;
+        });
   }
 
   onGoToNewsSinglePage(article) {
