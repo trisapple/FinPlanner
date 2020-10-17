6c653b7524849cd11531623c0d1c7278334d6853
Tristan Chng <tristan.chng@gmail.com>
2020-10-17 13:08:04 +0800

    Add comments for accountslist

--- src/app/accounts/accountslist/accountslist.page.ts
+++ src/app/accounts/accountslist/accountslist.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ExpensesService } from 'src/app/expenses.service';
 import { NavController } from '@ionic/angular';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -10,21 +10,26 @@ import { SaltedgeService } from 'src/app/saltedge.service';
   templateUrl: './accountslist.page.html',
   styleUrls: ['./accountslist.page.scss'],
 })
-export class AccountslistPage implements OnInit {
+export class AccountslistPage {
+
+  balances = [] // currency Object to be pushed to this balances array (so that firebase can accept it)
+  currency = {} // Aggregated currencies from all accounts in the salt edge connection
+  balancescurrencycode = [] // currencycode Object to be pushed to this balancescurrencycode array (so that firebase can accept it)
+  currencycode = {} // Aggregated currencies from all accounts in the salt edge connection (with currency symbol)
 
   transactionhistory(account) {
     console.log(account);
-    this.saltedgeService.saltedgeaccount = account
-    this.expensesService.transactionhistorytitle = account.account_name;
-    this.saltedgeService.saltedgeaccountcurrencycode = account.currency_code
+    this.saltedgeService.saltedgeaccount = account // Set the account information to a global variable so that we could access it from anywhere
+    this.expensesService.transactionhistorytitle = account.account_name; // Display account name on top menu bar
+    this.saltedgeService.saltedgeaccountcurrencycode = account.currency_code // Set the currency code so that we could display the proper symbol
     this.navCtrl.navigateForward(['/accounts/transactionhistory']);
   }
 
   // Onclick to next page, passing account information to the next page
   spendinginsights(account) {
-    this.saltedgeService.saltedgeaccount = account
-    this.expensesService.transactionhistorytitle = account.account_name
-    this.saltedgeService.saltedgeaccountcurrencycode = account.currency_code
+    this.saltedgeService.saltedgeaccount = account // Set the account information to a global variable so that we could access it from anywhere
+    this.expensesService.transactionhistorytitle = account.account_name // Display account name on top menu bar
+    this.saltedgeService.saltedgeaccountcurrencycode = account.currency_code // Set the currency code so that we could display the proper symbol
     this.navCtrl.navigateForward(['/accounts/spendinginsights']); // Navigate to the next page
   }
 
@@ -76,48 +81,39 @@ export class AccountslistPage implements OnInit {
       res.on("end", (chunk) => {
         var body = Buffer.concat(chunks);
         console.log(JSON.parse(body.toString()));
-        saltedgeService.saltedgeaccounts = JSON.parse(body.toString())["data"]
+        saltedgeService.saltedgeaccounts = JSON.parse(body.toString())["data"] // Get all accounts from a salt edge connection
+        console.log(saltedgeService.saltedgeaccounts)
 
-        var balances = []
-        var currency = {}
-        var balancescurrencycode = []
-        var currencycode = {}
-
-        for (let account of JSON.parse(body.toString())["data"]) {
-          if (currency[account.currency_code] == undefined) {
-            currency[account.currency_code] = 0 // Start from 0
+        // Loop through all accounts in the salt edge connection
+        for (let account of saltedgeService.saltedgeaccounts) {
+          // If the currency is not yet added to the currency Object
+          if (this.currency[account.currency_code] == undefined) {
+            this.currency[account.currency_code] = 0 // Start from 0
           }
-          currency[account.currency_code] += account.balance
+          // Add it to the relevant currency key
+          this.currency[account.currency_code] += account.balance
         }
-        for (let eachcurrency of Object.keys(currency)) {
-          currencycode[eachcurrency] = currency[eachcurrency].toLocaleString('en-SG', { style: 'currency', currency: eachcurrency })
+        // Loop through the currency codes in the currency object and add the currency symbol to another array
+        for (let eachcurrency of Object.keys(this.currency)) {
+          this.currencycode[eachcurrency] = this.currency[eachcurrency].toLocaleString('en-SG', { style: 'currency', currency: eachcurrency })
         }
-        // each["balance"].toLocaleString('en-SG', { style: 'currency', currency: each["currency_code"] })
-        balances.push(currency)
-        balancescurrencycode.push(currencycode)
-        console.log(currency)
+        this.balances.push(this.currency)
+        this.balancescurrencycode.push(this.currencycode)
+        console.log(this.currency)
 
         if (userService.loggedin == false) {
-          firestore.collection('users').doc("test1234@example.com").collection("saltedgeconnections").doc(saltedgeService.saltedgeconnection["id"]).set({
-            connectioninfo: JSON.parse(body.toString())["data"],
-            balances: balances,
-            balancescurrencycode: balancescurrencycode
-          }, { merge: true })
+          this.aggregateaccountsinconnection("test1234@example.com")
         } else {
-          firestore.collection('users').doc(this.userService.uid).collection("saltedgeconnections").doc(saltedgeService.saltedgeconnection["id"]).set({
-            connectioninfo: JSON.parse(body.toString())["data"],
-            balances: balances,
-            balancescurrencycode: balancescurrencycode
-          }, { merge: true })
+          this.aggregateaccountsinconnection(this.userService.uid)
         }
 
         // Loop through the accounts to get the account name (or nature) and the balance
         for (let account of saltedgeService.saltedgeaccounts) {
 
           if (account["extra"]["account_name"]) {
-            account["account_name"] = account["extra"]["account_name"]
+            account["account_name"] = account["extra"]["account_name"] // Display the account name if there is
           } else {
-            account["account_name"] = expensesService.humanize(account["nature"])
+            account["account_name"] = expensesService.humanize(account["nature"]) // Otherwise display the nature of account
           }
           account["balance"] = account["balance"].toLocaleString('en-SG', { style: 'currency', currency: account["currency_code"] }) // Include currency symbol
           account["expanded"] = false // Allow the expandable to work
@@ -133,8 +129,12 @@ export class AccountslistPage implements OnInit {
     req.end();
   }
 
-  ngOnInit() {
-
+  aggregateaccountsinconnection(uid) {
+    this.firestore.collection('users').doc(uid).collection("saltedgeconnections").doc(this.saltedgeService.saltedgeconnection["id"]).set({
+      connectioninfo: this.saltedgeService.saltedgeaccounts,
+      balances: this.balances,
+      balancescurrencycode: this.balancescurrencycode
+    }, { merge: true })
   }
 
 }
