65324975ca4e1a27129ff05f4c22eb9824516dc2
fishsoupp <50898157+fishsoupp@users.noreply.github.com>
2020-11-08 23:09:49 +0800

    half baked fried rice

--- src/app/courses/courses.page.ts
+++ src/app/courses/courses.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -8,20 +9,35 @@ import { NavController } from '@ionic/angular';
 })
 export class CoursesPage implements OnInit {
 
-  // public cards: Array<{ title: string, subtitle: string, description: string }> = [
-  //   { 
-  //      title : 'What is financial planning?',
-  //      subtitle: 'Hello',
-  //      description : 'Financial planning is the process of seeking to meet your life goals through the proper management of your finances. Financial planning helps you make advance provision for financial needs that will arise in the future. The objective of financial planning is to ensure that the right amount of money is available in the right hands at the right point in the future to achieve an individual\'s life goals.',
-  //   },
-  //   { 
-  //      title : 'Why should I make a financial plan?',
-  //      subtitle: 'Mary',
-  //      description : 'Financial planning provides direction and meaning to your financial decisions. It allows you to understand how each financial decision you make affects other areas of your finances. For example, buying a particular investment product might help you save adequately to finance your child\'s higher education, or it may provide enough for a comfortable retirement. You can also adapt more easily to life changes and feel more secure that your goals are on track.',
-  //   },
+  public items = [
+    {
+      pic: '../../assets/plan.jpg',
+      title: 'Financial Planning and Basics',
+    },
+    {
+      pic: '../../assets/invest.jpg',
+      title: 'Investing Basics',
+    },
+    {
+      pic: '../../assets/save.png',
+      title: 'Tips on Saving Money',
+    },
+    {
+      pic: '../../assets/etf.jpg',
+      title: 'How to Invest (The Right Way) with ETFs',
+    },
+    {
+      pic: '../../assets/retire.jpg',
+      title: 'How to Plan for Your Retirement',
+    },
+    {
+      pic: '../../assets/crypto.jpg',
+      title: 'Investing with Cryptocurrency',
+    }
+  ];
 
-  // ];
-  constructor(public navCtrl: NavController) {}
+
+  constructor(public navCtrl: NavController, private router: Router) {}
 
   ngOnInit() {
   }
