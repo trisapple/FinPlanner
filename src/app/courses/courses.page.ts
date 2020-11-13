f564a509656dcffbef2f89cde2fa5052d07e8fb3
Aloy23 <aloysiustws@gmail.com>
2020-11-13 16:20:50 +0800

    Added "Keep me logged in" code in Courses Page.

--- src/app/courses/courses.page.ts
+++ src/app/courses/courses.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import * as firebase from 'firebase';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-courses',
@@ -37,7 +40,42 @@ export class CoursesPage implements OnInit {
   ];
 
 
-  constructor(public navCtrl: NavController, private router: Router) {}
+  constructor(public navCtrl: NavController, private router: Router, private userService: UserService) {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user != null) {
+        let sub: Subscription = userService.login(user.uid).subscribe((data) => {
+          userService.loggedin = true;
+          userService.name = data["name"];
+          userService.email = user.email;
+          userService.uid = user.uid;
+          // userService.provider = "Email and Password"
+          if (user.providerData[0]["providerId"] == "password") {
+            userService.provider = "Email and Password";
+          }
+          if (user.providerData[0]["providerId"] == "google.com") {
+            userService.socialLogin = true;
+            userService.provider = "Google";
+            userService.profilePicture = user.providerData[0]["photoURL"];
+          }
+          if (user.providerData[0]["providerId"] == "facebook.com") {
+            userService.socialLogin = true;
+            userService.provider = "Facebook";
+            userService.profilePicture = user.providerData[0]["photoURL"];
+          }
+          console.log(user);
+          // if (this.activatedRoute.snapshot.queryParamMap.get("connection_id")) {
+          //   this.connection_id()
+          // } else {
+          //   this.getsaltedgedata()
+          // }
+          sub.unsubscribe();
+        });
+      } else {
+        // No user is signed in.
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
   ngOnInit() {
   }
