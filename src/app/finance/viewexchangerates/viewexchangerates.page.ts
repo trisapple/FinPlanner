a085c53b51ae54f4edd2fd24ce7bc7e9e5f74a5b
Tristan Chng <tristan.chng@gmail.com>
2020-11-28 16:02:09 +0800

    exchangerates.io api in view exchange rates page

--- src/app/finance/viewexchangerates/viewexchangerates.page.ts
+++ src/app/finance/viewexchangerates/viewexchangerates.page.ts
@@ -13,6 +13,7 @@ import { NewsService } from '../../news.service';
 export class ViewexchangeratesPage {
   symbol: any;
   params: any;
+  SGDtoUSDrate = 0
   constructor(private newsService: NewsService, private expensesService: ExpensesService, private userService: UserService, private router: Router, private route: ActivatedRoute) {
     this.route.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
@@ -23,26 +24,56 @@ export class ViewexchangeratesPage {
 
       var options = {
         'method': 'GET',
-        'hostname': 'www.alphavantage.co',
-        'path': `/query?function=GLOBAL_QUOTE&symbol=${this.params.symbol}&interval=5min&apikey=JDK7QWBWHQDIT41Y`,
-        'headers': {
-        },
+        'hostname': 'api.exchangeratesapi.io',
+        'path': '/latest?base=SGD',
         'maxRedirects': 20
       };
 
-      var req = https.request(options, function (res) {
+      var req = https.request(options, (res) => {
         var chunks = [];
 
         res.on("data", function (chunk) {
           chunks.push(chunk);
         });
 
-        res.on("end", function (chunk) {
+        res.on("end", (chunk) => {
           var body = Buffer.concat(chunks);
-          console.log(JSON.parse(body.toString())["Global Quote"])
-          
-          expensesService.Stocks = JSON.parse(body.toString())["Global Quote"]
-          
+          console.log(JSON.parse(body.toString()));
+
+          this.SGDtoUSDrate = JSON.parse(body.toString())["rates"]["USD"] // Get the price of USD for 1 SGD
+
+          var https = require('follow-redirects').https;
+
+          var options = {
+            'method': 'GET',
+            'hostname': 'www.alphavantage.co',
+            'path': `/query?function=GLOBAL_QUOTE&symbol=${this.params.symbol}&interval=5min&apikey=JDK7QWBWHQDIT41Y`,
+            'headers': {
+            },
+            'maxRedirects': 20
+          };
+
+          var req = https.request(options, function (res) {
+            var chunks = [];
+
+            res.on("data", function (chunk) {
+              chunks.push(chunk);
+            });
+
+            res.on("end", function (chunk) {
+              var body = Buffer.concat(chunks);
+              console.log(JSON.parse(body.toString())["Global Quote"])
+
+              expensesService.Stocks = JSON.parse(body.toString())["Global Quote"]
+
+            });
+
+            res.on("error", function (error) {
+              console.error(error);
+            });
+          });
+
+          req.end();
         });
 
         res.on("error", function (error) {
@@ -51,6 +82,8 @@ export class ViewexchangeratesPage {
       });
 
       req.end();
+
+
     });
   }
 }
